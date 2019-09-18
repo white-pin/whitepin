4e2647c7840f2162ff393fa7390c247f0a3344b3
김상열 <sangkimyul@ibizsoftware.net>
2019-09-18 11:43:56 +0900

    - swagger resource add

--- server/src/main/java/com/github/whitepin/server/config/WebMvcConfig.java
+++ server/src/main/java/com/github/whitepin/server/config/WebMvcConfig.java
@@ -3,6 +3,7 @@ package com.github.whitepin.server.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -14,6 +15,12 @@ public class WebMvcConfig implements WebMvcConfigurer {
         registry.addMapping("/**")
                 .allowedOrigins("*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE");
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("swagger-ui.html").addResourceLocations("classpath:/META-INF/resources/");
+        registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
     }
 }
 
